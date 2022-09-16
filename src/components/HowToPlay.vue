f69de4f9c0e3f487686bfd212fe338c4fd303cbd
SonGokussj4 <SonGokussj4@centrum.cz>
2022-09-16 15:33:59 +0200

    Clean Home page, add How to play description as a separate component

---
+++ src/components/HowToPlay.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import { RouterLink } from "vue-router"
+import WelcomeItem from "./WelcomeItem.vue";
+import CommunityIcon from "./icons/IconCommunity.vue";
+
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+
+    <template #heading>How to play</template>
+
+    <p>Press a button to start the game. The game will get 6 random people from the internet.</p>
+    <p>Then you choose a person on the left and person on the right. And you play 🤘🧻✂.</p>
+    <p>Leaderboard table can be viewed in the <RouterLink to="/scores">Scores</RouterLink> page</p>
+
+    <br />
+  </WelcomeItem>
+
+</template>
